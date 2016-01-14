11b9cd0b8ab41a883c682f23975d798964fd80b1
Ben Shukman <shukipost@gmail.com>
2016-01-13 18:12:41 -0800

    Initial commit

---
+++ motion.ino
@@ -0,0 +1,12 @@
+// motion control implementation
+// compiled by Ben Shukman
+
+// kalman filters, angle calculation, sensor noise reduction, PID control, motor control, remote control
+
+void setup() {
+
+}
+
+void loop() {
+
+}
