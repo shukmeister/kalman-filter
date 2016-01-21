e686156d4c81675518a284a41c23d149db2192c0
Ben Shukman <shukipost@gmail.com>
2016-01-20 16:47:14 -0800

    Rotational output working

--- motion/motion.ino
+++ motion/motion.ino
@@ -119,144 +119,145 @@ void setup() {
 	// accZ = (i2cData[4] << 8) | i2cData[5];
 }
 
-void loop() {
-	Wire.beginTransmission(0x68);
-	Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-	Wire.endTransmission(false);
-	Wire.requestFrom(0x68,14,true);  // request a total of 14 registers
-	AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-	AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-	AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-	Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-	GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-	GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-	GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-	Serial.print("AcX = "); Serial.print(AcX);
-	Serial.print(" | AcY = "); Serial.print(AcY);
-	Serial.print(" | AcZ = "); Serial.print(AcZ);
-	Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-	Serial.print(" | GyX = "); Serial.print(GyX);
-	Serial.print(" | GyY = "); Serial.print(GyY);
-	Serial.print(" | GyZ = "); Serial.println(GyZ);
-	delay(333);
-}
-
 // void loop() {
-// 	// Serial.print(F("i2cRead failed: "));
-// 	// Serial.println("asdf");
-// 	// while (i2cRead(0x3B, i2cData, 14));
-// 	// for (uint8_t i = 0; i < 15; i++) {
-// 	// 	Serial.println(i2cData[i]);
-// 	// }
-
-// 	while (i2cRead(0x3B, i2cData, 14));
-// 	accX = ((i2cData[0] << 8) | i2cData[1]);
-// 	accY = ((i2cData[2] << 8) | i2cData[3]);
-// 	accZ = ((i2cData[4] << 8) | i2cData[5]);
-// 	tempRaw = (i2cData[6] << 8) | i2cData[7];
-// 	gyroX = (i2cData[8] << 8) | i2cData[9];
-// 	gyroY = (i2cData[10] << 8) | i2cData[11];
-// 	gyroZ = (i2cData[12] << 8) | i2cData[13];
-
-// 	Serial.print("AcX = "); Serial.print(accX);
-// 	Serial.print(" | AcY = "); Serial.print(accY);
-// 	Serial.print(" | AcZ = "); Serial.print(accZ);
-// 	Serial.print(" | Tmp = "); Serial.print(tempRaw/340.00+36.53);  //equation for temperature in degrees C from datasheet
-// 	Serial.print(" | GyX = "); Serial.print(gyroX);
-// 	Serial.print(" | GyY = "); Serial.print(gyroY);
-// 	Serial.print(" | GyZ = "); Serial.println(gyroZ);
-
-// 	// double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
-// 	// timer = micros();
-
-// 	// #ifdef RESTRICT_PITCH // Eq. 25 and 26
-// 	//   double roll  = atan2(accY, accZ) * RAD_TO_DEG;
-// 	//   double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
-// 	// #else // Eq. 28 and 29
-// 	//   double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
-// 	//   double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
-// 	// #endif
-
-// 	// double gyroXrate = gyroX / 131.0; // Convert to deg/s
-// 	// double gyroYrate = gyroY / 131.0; // Convert to deg/s
-
-// 	// #ifdef RESTRICT_PITCH
-// 	//   // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-// 	//   if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
-// 	//     kalmanX.setAngle(roll);
-// 	//     compAngleX = roll;
-// 	//     kalAngleX = roll;
-// 	//     gyroXangle = roll;
-// 	//   } else
-// 	//     kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
-
-// 	//   if (abs(kalAngleX) > 90)
-// 	//     gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
-// 	//   kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
-// 	// #else
-// 	//   // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-// 	//   if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
-// 	//     kalmanY.setAngle(pitch);
-// 	//     compAngleY = pitch;
-// 	//     kalAngleY = pitch;
-// 	//     gyroYangle = pitch;
-// 	//   } else
-// 	//     kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
-
-// 	//   if (abs(kalAngleY) > 90)
-// 	//     gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
-// 	//   kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
-// 	// #endif
-
-// 	//   gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
-// 	//   gyroYangle += gyroYrate * dt;
-// 	//   //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
-// 	//   //gyroYangle += kalmanY.getRate() * dt;
-
-// 	//   compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
-// 	//   compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
-
-// 	//   // Reset the gyro angle when it has drifted too much
-// 	//   if (gyroXangle < -180 || gyroXangle > 180)
-// 	//     gyroXangle = kalAngleX;
-// 	//   if (gyroYangle < -180 || gyroYangle > 180)
-// 	//     gyroYangle = kalAngleY;
-
-// 	//   /* Print Data */
-// 	// #if 1 // Set to 1 to activate
-// 	//   Serial.print(accX); Serial.print("\t");
-// 	//   Serial.print(accY); Serial.print("\t");
-// 	//   Serial.print(accZ); Serial.print("\t");
-
-// 	//   Serial.print(gyroX); Serial.print("\t");
-// 	//   Serial.print(gyroY); Serial.print("\t");
-// 	//   Serial.print(gyroZ); Serial.print("\t");
-
-// 	//   Serial.print("\t");
-// 	// #endif
-
-// 	//   Serial.print("roll: "); Serial.print(roll); Serial.print("\t");
-// 	//   Serial.print(gyroXangle); Serial.print("\t");
-// 	//   Serial.print(compAngleX); Serial.print("\t");
-// 	//   Serial.print(kalAngleX); Serial.print("\t");
-
-// 	//   Serial.print("\t");
-
-// 	//   Serial.print(pitch); Serial.print("\t");
-// 	//   Serial.print(gyroYangle); Serial.print("\t");
-// 	//   Serial.print(compAngleY); Serial.print("\t");
-// 	//   Serial.print(kalAngleY); Serial.print("\t");
-
-// 	// #if 0 // Set to 1 to print the temperature
-// 	//   Serial.print("\t");
-
-// 	//   double temperature = (double)tempRaw / 340.0 + 36.53;
-// 	//   Serial.print(temperature); Serial.print("\t");
-// 	// #endif
-
-// 	//   Serial.print("\r\n");
-// 	//   delay(2);
+// 	Wire.beginTransmission(0x68);
+// 	Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+// 	Wire.endTransmission(false);
+// 	Wire.requestFrom(0x68,14,true);  // request a total of 14 registers
+// 	AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+// 	AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+// 	AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+// 	Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+// 	GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+// 	GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+// 	GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+// 	Serial.print("AcX = "); Serial.print(AcX);
+// 	Serial.print(" | AcY = "); Serial.print(AcY);
+// 	Serial.print(" | AcZ = "); Serial.print(AcZ);
+// 	Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+// 	Serial.print(" | GyX = "); Serial.print(GyX);
+// 	Serial.print(" | GyY = "); Serial.print(GyY);
+// 	Serial.print(" | GyZ = "); Serial.println(GyZ);
+// 	delay(333);
 // }
+
+void loop() {
+	// Serial.print(F("i2cRead failed: "));
+	// Serial.println("asdf");
+	// while (i2cRead(0x3B, i2cData, 14));
+	// for (uint8_t i = 0; i < 15; i++) {
+	// 	Serial.println(i2cData[i]);
+	// }
+
+	while (i2cRead(0x3B, i2cData, 14));
+	accX = ((i2cData[0] << 8) | i2cData[1]);
+	accY = ((i2cData[2] << 8) | i2cData[3]);
+	accZ = ((i2cData[4] << 8) | i2cData[5]);
+	tempRaw = (i2cData[6] << 8) | i2cData[7];
+	gyroX = (i2cData[8] << 8) | i2cData[9];
+	gyroY = (i2cData[10] << 8) | i2cData[11];
+	gyroZ = (i2cData[12] << 8) | i2cData[13];
+
+	// Serial.print("AcX = "); Serial.print(accX);
+	// Serial.print(" | AcY = "); Serial.print(accY);
+	// Serial.print(" | AcZ = "); Serial.print(accZ);
+	// Serial.print(" | Tmp = "); Serial.print(tempRaw/340.00+36.53);  //equation for temperature in degrees C from datasheet
+	// Serial.print(" | GyX = "); Serial.print(gyroX);
+	// Serial.print(" | GyY = "); Serial.print(gyroY);
+	// Serial.print(" | GyZ = "); Serial.println(gyroZ);
+
+	double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+	timer = micros();
+
+	#ifdef RESTRICT_PITCH // Eq. 25 and 26
+	  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+	  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+	#else // Eq. 28 and 29
+	  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+	  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+	#endif
+
+	double gyroXrate = gyroX / 131.0; // Convert to deg/s
+	double gyroYrate = gyroY / 131.0; // Convert to deg/s
+
+	#ifdef RESTRICT_PITCH
+	  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+	  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+	    kalmanX.setAngle(roll);
+	    compAngleX = roll;
+	    kalAngleX = roll;
+	    gyroXangle = roll;
+	  } else
+	    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+
+	  if (abs(kalAngleX) > 90)
+	    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+	  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+	#else
+	  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+	  if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+	    kalmanY.setAngle(pitch);
+	    compAngleY = pitch;
+	    kalAngleY = pitch;
+	    gyroYangle = pitch;
+	  } else
+	    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+
+	  if (abs(kalAngleY) > 90)
+	    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+	  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+	#endif
+
+	  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+	  gyroYangle += gyroYrate * dt;
+	  //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+	  //gyroYangle += kalmanY.getRate() * dt;
+
+	  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+	  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+
+	  // Reset the gyro angle when it has drifted too much
+	  if (gyroXangle < -180 || gyroXangle > 180)
+	    gyroXangle = kalAngleX;
+	  if (gyroYangle < -180 || gyroYangle > 180)
+	    gyroYangle = kalAngleY;
+
+	  /* Print Data */
+	#if 0 // Set to 1 to activate
+	  Serial.print(accX); Serial.print("\t");
+	  Serial.print(accY); Serial.print("\t");
+	  Serial.print(accZ); Serial.print("\t");
+
+	  Serial.print(gyroX); Serial.print("\t");
+	  Serial.print(gyroY); Serial.print("\t");
+	  Serial.print(gyroZ); Serial.print("\t");
+
+	  Serial.print("\t");
+	#endif
+
+	  Serial.print("roll: "); Serial.print(roll); Serial.print("\n");
+	  Serial.print("gyroXangle: ");Serial.print(gyroXangle); Serial.print("\n");
+	  Serial.print("compAngleX: ");Serial.print(compAngleX); Serial.print("\n");
+	  Serial.print("kalAngleX: ");Serial.print(kalAngleX); Serial.print("\n");
+
+	  Serial.print("\n");
+
+	  Serial.print("pitch: ");Serial.print(pitch); Serial.print("\n");
+	  Serial.print("gyroYangle: ");Serial.print(gyroYangle); Serial.print("\n");
+	  Serial.print("compAngleY: ");Serial.print(compAngleY); Serial.print("\n");
+	  Serial.print("kalAngleY: ");Serial.print(kalAngleY); Serial.print("\n");
+
+	#if 0 // Set to 1 to print the temperature
+	  Serial.print("\t");
+
+	  double temperature = (double)tempRaw / 340.0 + 36.53;
+	  Serial.print(temperature); Serial.print("\t");
+	#endif
+
+	  Serial.print("\r\n");
+	  delay(2);
+	  delay(333);
+}
 
 // old code below:
 
